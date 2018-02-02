c39c55f75d56cc60d01cf81517e73b1fb4989a1d
Evan Garrett <evang522@gmail.com>
2018-02-02 15:21:53 -0500

    half-way through recipe router tests

---
+++ test/test-recipes.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+// this lets us use *expect* style syntax in our tests
+// so we can do things like `expect(1 + 1).to.equal(2);`
+// http://chaijs.com/api/bdd/
+const expect = chai.expect;
+
+// This let's us make HTTP requests
+// in our tests.
+// see: https://github.com/chaijs/chai-http
+chai.use(chaiHttp);
+
+
+
+
+describe('Recipe-router-Tests', function() {
+  before(function () {
+    return runServer();
+  })
+
+  after(function() {
+    return closeServer();
+  })
+
+
+
+
+
+
+
+})
